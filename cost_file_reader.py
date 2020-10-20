6101006451019d1e42f09d83c59365002245ada1
Sidharth Yatish <Sidharth.Yathish@genesys.com>
2020-10-20 10:57:10 +0530

    Created function to read costs file

---
+++ cost_file_reader.py
@@ -0,0 +1,16 @@
+import json
+
+
+def read_cost(file_name):
+    try:
+        with open(file_name) as file:
+            cost_data = json.load(file)
+        return cost_data
+    except FileNotFoundError:
+        return {"ERROR": "File not found"}
+    except ValueError:
+        return {"ERROR" : "Invalid input data"}
+
+
+if __name__ == '__main__':
+    print(read_cost('./cost_file.json'))
